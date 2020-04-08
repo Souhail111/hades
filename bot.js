c5cc6f4bd42c671b5edcc9d808d93adfe22298b4
Souhail111 <62572439+Souhail111@users.noreply.github.com>
2020-04-08 13:08:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,7 +63,7 @@ message.channel.sendEmbed(embed);
 /*blacklist*/
 
 client.on('guildMemberAdd', (mem1) => {
-             if ( (mem1.user.id == "653310475534336027") || (mem1.user.id == "668564648747860001") || (mem1.user.id == "") || (mem1.user.id == "") || (mem1.user.id == "")  || (mem1.user.id == "")|| (mem1.user.id == "")|| (mem1.user.id == "")|| (mem1.user.id == "")|| (mem1.user.id == "")){
+             if ( (mem1.user.id == "653310475534336027") || (mem1.user.id == "668564648747860001") || (mem1.user.id == "697022685221486623") || (mem1.user.id == "") || (mem1.user.id == "")  || (mem1.user.id == "")|| (mem1.user.id == "")|| (mem1.user.id == "")|| (mem1.user.id == "")|| (mem1.user.id == "")){
 		let chan = mem1.guild.channels.find( channel => channel.id === "620781295563177985");
                 chan.send("__**Warning :warning: :**__**This member ``" + mem1.displayName + "`` is blacklisted:rotating_light: ** ");
 		mem1.ban(7) 
@@ -78,7 +78,7 @@ if (msg.content.startsWith(prefix + 'scann')) {
 
 msg.guild.ban('653310475534336027').then(user => msg.channel.send(`**🚨A Blacklisted user has been detected :** __** ${user.username || user.id || user}**__ **has been banned from**🔨 __** ${msg.guild.name} **__ `));
 msg.guild.ban('668564648747860001').then(user => msg.channel.send(`**🚨A Blacklisted user has been detected :** __** ${user.username || user.id || user}**__ **Has been banned from**🔨 __** ${msg.guild.name} **__ `));
-msg.guild.ban('').then(user => msg.channel.send(`**🚨A Blacklisted user has been detected :** __** ${user.username || user.id || user}**__ **Has been banned from**🔨 __** ${msg.guild.name} **__ `))
+msg.guild.ban('697022685221486623').then(user => msg.channel.send(`**🚨A Blacklisted user has been detected :** __** ${user.username || user.id || user}**__ **Has been banned from**🔨 __** ${msg.guild.name} **__ `))
    
 	
 .then(user2 => msg.channel.send('__**The server is clean**__✅'));
